57715f36bdea30a083318b1d6257b1e7421f1b2e
Andrey Minin <ewkjf@mail.ru>
2022-05-29 19:44:13 +0400

    added comments to the code

--- files-explorer/src/file_explorer.rs
+++ files-explorer/src/file_explorer.rs
@@ -3,6 +3,9 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::path::Path;
 
+/// A function that given a directory, recursively finds all files with a
+/// given file extension in that directory and all sub-directories,
+/// and counts the number of lines in the file and prints it to stdout.
 pub fn explore_files_recursively(dir: &String, file_extension: &String) {
     let lines_amount = WalkDir::new(dir)
         .into_iter()
@@ -13,12 +16,14 @@ pub fn explore_files_recursively(dir: &String, file_extension: &String) {
     println!("Total line amount: {}", lines_amount);
 }
 
+/// Function to filter out the filenames, that only have a given file extension at the end
 fn ends_with(e: &DirEntry, file_extension: &String) -> bool {
     return e.file_name()
         .to_string_lossy()
         .ends_with(file_extension);
 }
 
+/// Function to count lines in the file, located at the provided filepath
 fn count_lines(path: &Path) -> usize {
     return BufReader::new(File::open(path)
         .expect("Failed to open file"))
